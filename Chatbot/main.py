d74a9547948586a089f1fb46e2147b1018e2cac0
Avnish Yadav <149303613+ianishyadav103@users.noreply.github.com>
2024-01-19 14:28:25 +0530

    Add files via upload

---
+++ Chatbot/main.py
@@ -0,0 +1,106 @@
+from nltk.chat.util import Chat, reflections
+
+reflections = {
+  "i am"       : "you are",
+  "i was"      : "you were",
+  "i"          : "you",
+  "i'm"        : "you are",
+  "i'd"        : "you would",
+  "i've"       : "you have",
+  "i'll"       : "you will",
+  "my"         : "your",
+  "you are"    : "I am",
+  "you were"   : "I was",
+  "you've"     : "I have",
+  "you'll"     : "I will",
+  "your"       : "my",
+  "yours"      : "mine",
+  "you"        : "me",
+  "me"         : "you"
+}
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ], 
+    [
+        r"what is your name ?",
+        ["I am a bot created by Avnish using reference avaible on internet. you can call me Chatterbot!",]
+    ],
+    [
+        r"how are you ?",
+        ["As a computer program, I don't have feelings. How about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"I am fine",
+        ["Great to hear that, How can I help you?",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","How can I help you?:)",]
+    ],
+    [
+        r"(.*) age?",
+        ["As a computer, I don't age like fine wine; I'm more like eternal lines of code. If I had a birthday, it would probably be January 1, 1970, the start of the Unix epoch. But who's counting? 😉 ",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Pune, Maharashtra Pradesh',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
+    ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["John Cena"]
+    ],
+    [
+        r"quit",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [ 
+        r'(.*)', 
+        ["I'm sorry, I didn’t understand that."]
+    ],
+]
+
+def chat():
+    print("Hi! I am a chatbot for your service")
+    chat = Chat(pairs,reflections)
+    chat.converse()
+#initiate the conversation
+if __name__ == "__main__":
+    chat()
